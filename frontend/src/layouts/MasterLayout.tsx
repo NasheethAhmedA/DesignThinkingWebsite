d604d18ef11dd3bdb60b1426c539681e40f6bd35
Nasheeth Ahmed A <nasheethahmeda04@gmail.com>
2025-04-30 23:29:04 +0530

    static build changes
    changed some image paths to relative imports for a better build of static site.

--- frontend/src/layouts/MasterLayout.tsx
+++ frontend/src/layouts/MasterLayout.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import DesignThinkingLogo from '/assets/DesignThinkingLogo.png';
+import KingstonLogo from '/assets/kingston_logo.png';
 
 const MasterLayout: React.FC = () => {
   return (
@@ -9,14 +11,14 @@ const MasterLayout: React.FC = () => {
       <header className="bg-white shadow-md">
         <div className="container mx-auto px-4 py-6 flex justify-around items-center">
         <div className="flex items-center hidden md:flex">
-              <img src="./././assets/DesignThinkingLogo.png" alt="Logo" className="h-24 w-24" />
+              <img src={DesignThinkingLogo} alt="Logo" className="h-24 w-24" />
             </div>
             <div className="flex flex-col items-center">
             <span className="text-5xl font-bold text-blue-800 text-center">Design Thinking Club</span>
             <span className="text-3xl font-bold text-blue-800 text-center hidden md:block">Kingston Engineering College</span>
             </div>
           <div className="hidden md:block">
-          <img src="./././assets/kingston_logo.png" alt="Logo" className="h-24" />
+          <img src={KingstonLogo} alt="Logo" className="h-24" />
           </div>
         </div>
         <Navbar />
